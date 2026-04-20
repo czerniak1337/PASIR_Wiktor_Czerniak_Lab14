20658e307d0637948dffdbdd9ecedb3df3ea183e
piopol <piotr.polinski@student.pk.edu.pl>
2026-04-20 18:27:03 +0200

    initial commit

---
+++ src/main/java/pk/wc/pasir_wiktor_czerniak/PasirWiktorCzerniakApplication.java
@@ -0,0 +1,13 @@
+package pk.wc.pasir_wiktor_czerniak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasirWiktorCzerniakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasirWiktorCzerniakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pk/wc/pasir_wiktor_czerniak/PasirWiktorCzerniakApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.wc.pasir_wiktor_czerniak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasirWiktorCzerniakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
